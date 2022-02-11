f18830eff0a65e1e90ca219e96616afb67bdc248
Leonardo Medeiros <leonardo@peek.us>
2022-02-11 18:23:22 -0300

    updated some projects and added new layout

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,15 @@
+// var i = 0;
+// var txt = 'Lorem ipsum typing effect!'; /* The text */
+// var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("demo").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
 (function($){
 
 	/* ---------------------------------------------- /*
@@ -8,8 +20,6 @@
 		$('#loading').fadeOut();
 		$('#loading').delay(350).fadeOut('slow');
 	});
-
-
 
 		/* ---------------------------------------------- /*
 		 * Animated scrolling / Scroll Up
