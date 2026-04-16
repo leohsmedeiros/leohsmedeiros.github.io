ef428f227bcead38ffea2d6879d3c2e5bdb5b327
Leonardo Medeiros <leohsmedeiros@gmail.com>
2026-04-16 14:14:50 -0300

    Fix navbar's background

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,13 +34,24 @@
 			e.preventDefault();
 		});
 
+		function updateNavbarScrollState() {
+			if ($(window).scrollTop() > 0) {
+				$('.header nav').addClass('bg-background/80 backdrop-blur-xl border-b border-border/50').removeClass('bg-transparent');
+			} else {
+				$('.header nav').removeClass('bg-background/80 backdrop-blur-xl border-b border-border/50').addClass('bg-transparent');
+			}
+		}
+
 		$(window).scroll(function() {
 			if ($(this).scrollTop() > 100) {
 				$('.scroll-up').fadeIn();
 			} else {
 				$('.scroll-up').fadeOut();
 			}
+			updateNavbarScrollState();
 		});
+
+		updateNavbarScrollState();
 
 		/* ---------------------------------------------- /*
 		 * Navbar
